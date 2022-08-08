32f5be200d740fc7bb8900d38a813d4ac604819a
dafiqrahman <46261435+dafiqrahman@users.noreply.github.com>
2022-08-08 19:46:06 +0700

    Update home.py

--- home.py
+++ home.py
@@ -1,10 +1,4 @@
 import streamlit as st
-import cv2
-import artifact.model as m
-from PIL import Image
-import torch
-from streamlit_webrtc import webrtc_streamer
-import av
 
 st.markdown("<h1 style='text-align: center;'>Mask Recognition Application</h1>",
             unsafe_allow_html=True)
